4400c8d362db163463a4347f47044ce30ab80205
Jason-19 <jasonnunez02016@gmail.com>
2025-04-06 20:18:54 -0500

    Methods Users, Created Class Controller

--- routes/User.py
+++ routes/User.py
@@ -4,40 +4,40 @@ from models.UserModel import User, Order
 
 user_route = Blueprint('users',__name__) 
 
-class UsersController():
+class UsersController:
+    # @staticmethod
     def get_user(db:Session):
-        return db.query(User).all()
+        user= db.query(User).all()
+        if not user:
+            return jsonify({"message": "There are no users is empty"}), 404
+        return jsonify([user.serialize() for user in user]), 200
+    
+    
+    def getUserById(db:Session,user_id):
+        user= db.query(User).where(User.user_id == user_id).first()
+        
+        if not user:
+            return jsonify({"message": "User not found"}), 404
+        return jsonify(user.serialize()), 200
+    
+    
+    def getUserOrderById(db: Session, user_id):
+        orderUser= db.query(Order).where(User.user_id == user_id).all()
+        user_list = [user.serialize() for user in orderUser]
+        return jsonify(user_list), 200
+        
 
-    def create_user(db:Session, user):
-        db_user =User(**user.model_dump())
-        logs.info("adde",db_user)
-        print(db_user)
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
-
+ucontroller = UsersController
 
 @user_route.route("/users/all",methods=['GET'])
-def getuser():
-    user= db.query(User).all()
-    user_list = [user.serialize() for user in user]
-    return jsonify(user_list), 200
+def getUsersEndpoint():
+    return ucontroller.get_user(db)
 
-@user_route.route("/users/<id_user>/",methods=['GET'])
-def getuserById(id_user):
-    user= db.query(User).where(User.id_user == id_user).first()
-    return jsonify(user.serialize()), 200
+@user_route.route("/users/<user_id>/",methods=['GET'])
+def getUserByIdEndpoint(user_id):
+    return ucontroller.getUserById(db,user_id)
 
 
-@user_route.route("/users/order/<id_user>",methods=['POST'])
-def getuserOrderById(id_user):
-    orderUser= db.query(Order).where(User.id_user == id_user).all()
-    user_list = [user.serialize() for user in orderUser]
-    return jsonify(user_list), 200
-
-@user_route.route("/users",methods=['POST'])
-def createuser():
-    data = request.json
-    user = UsersController.create_user(db,data)
-    return jsonify(user.serialize()), 200
+@user_route.route("/users/order/<user_id>",methods=['POST'])
+def getuserOrderById(user_id):
+    return ucontroller.getUserOrderById(db,user_id)
