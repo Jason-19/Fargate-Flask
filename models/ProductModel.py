4fc7b1c8e8f7854ec22474e7eace4cb1f1659747
Jason-19 <jasonnunez02016@gmail.com>
2025-04-06 22:22:05 -0500

    New End Points Products (coins, VideoGames)

--- models/ProductModel.py
+++ models/ProductModel.py
@@ -4,25 +4,25 @@ from . import *
 class Product(Base):
     __tablename__ = "products"
     
-    id_product = Column(String(36), primary_key=True)
-    name = Column(String(255), nullable=False)
-    description = Column(JSON)
-    price = Column(Numeric(10, 2), nullable=False)
+    product_id = Column(String(50), primary_key=True)
+    prod_name = Column(String(50), nullable=False)
+    prod_description = Column(JSON)
+    prod_price = Column(Numeric(10, 2), nullable=False)
     image_url = Column(String(255))
     created_at = Column(TIMESTAMP, default=func.current_timestamp())
     
-    id_category = Column(String(36), ForeignKey('categories.category_id'))
+    category_id = Column(String(50), ForeignKey('categories.category_id'))
     category = relationship('Category', backref='products')
     
     def serialize(self):
         return{
-            "id_product": self.id_product,
-            "name": self.name,  
-            "description": self.description,
-            "price": self.price,
+            "product_id": self.product_id,
+            "name": self.prod_name,  
+            "description": self.prod_description,
+            "price": self.prod_price,
             "image_url":self.image_url,
             "created_at": self.created_at,
-            'id_category': self.id_category,
+            'id_category': self.category_id,
             'category_name': self.category.name
         }
 
